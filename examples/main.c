2537a99ae38b4fc7ba018f15bcc1104bbcc07d28
Drew Holzworth <drew.holzworth@csiro.au>
2021-10-01 12:45:44 +1000

    Update demo program to print wall clock time

--- examples/main.c
+++ examples/main.c
@@ -7,12 +7,24 @@
 // tmp
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
 
 #include "replacement.h"
 #include "apsimclient.h"
 
+double get_wall_time() {
+    struct timeval time;
+    if (gettimeofday(&time,NULL)){
+        //  Handle error
+        return 0;
+    }
+    return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
 int main(int argc, char** argv)
 {
+    double start_time = get_wall_time();
+
     // This is the name of the pipe as defined in the C# server code.
     // char *pipeName = "testpipe";
     char* ipAddress = argc > 1 ? argv[1] : "127.0.0.1";
@@ -38,23 +50,26 @@ int main(int argc, char** argv)
         replacement_t* change = createDoubleReplacement(path, value);
         printf("Running sims with the following changes:\n");
         printf("  %s = %.2f\n", path, value);
+        double run_clock = get_wall_time();
         runWithChanges(sock, &change, 1);
+        run_clock = get_wall_time() - run_clock;
+        fprintf(stdout, "Ran sims in %.2fms\n", run_clock * 1000);
+
         replacement_free(change);
 
         // Let's read some outputs.
-        char* table = "HarvestReport";
+        char* table = "Report";
         uint32_t nparams = 1;
         char* params[nparams];
         params[0] = "BiomassWt";
         // params[1] = "Sorghum.AboveGround.Wt";
         // params[2] = "Sorghum.Leaf.LAI";
         printf(" ");
-        clock_t t = clock();
+        double t = get_wall_time();
 
         output_t** outputs = readOutput(sock, table, params, nparams);
-        t = clock() - t;
-        double duration = 1000 * (double)t / CLOCKS_PER_SEC;
-        printf("Read %d outputs in %.2fms\n", nparams, duration);
+        t = get_wall_time() - t;
+        printf("Read %d outputs in %.2fms\n", nparams, t * 1000);
         for (uint32_t i = 0; i < nparams; i++) {
             uint32_t n = outputs[i]->len / sizeof(double); // all of our outputs are double for now
             printf("Received output %s with %u elements: [", params[i], n);
@@ -73,5 +88,8 @@ int main(int argc, char** argv)
     // Close the socket connection.
     printf("Disconnecting from server...\n");
     disconnectFromServer(sock);
+
+    double duration = get_wall_time() - start_time;
+    fprintf(stdout, "Total program execution: %.2fms\n", duration * 1000);
     return 0;
 }
